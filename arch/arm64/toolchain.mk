eb78a2cbed186db07b990a3270013231a63b3dd6
Taekgyun Ko <taeggyun.ko@samsung.com>
2018-01-26 07:51:46 +0000

    ARCH: set ARCH_arm64_TOOLCHAIN_PREFIX
    Change-Id: Id778774e8d70e567cd34b4aac4e1569577345b87
    Signed-off-by: Taekgyun Ko <taeggyun.ko@samsung.com>

--- arch/arm64/toolchain.mk
+++ arch/arm64/toolchain.mk
@@ -5,7 +5,7 @@ ifndef ARCH_arm64_TOOLCHAIN_PREFIX
 ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-elf-
 FOUNDTOOL=$(shell which $(ARCH_arm64_TOOLCHAIN_PREFIX)gcc)
 ifeq ($(FOUNDTOOL),)
-ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-linux-android-
+ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
 FOUNDTOOL=$(shell which $(ARCH_arm64_TOOLCHAIN_PREFIX)gcc)
 ifeq ($(FOUNDTOOL),)
 $(error cannot find toolchain, please set ARCH_arm64_TOOLCHAIN_PREFIX or add it to your path)
