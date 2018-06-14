17ec64f04b0d78b7c7694086b5e1459dfb4bfe64
Taekgyun Ko <taeggyun.ko@samsung.com>
2018-06-14 11:48:18 +0000

    APP: EXYNOS_BOOT: add normal/warm/WDT reset booting code
    Change-Id: If23e450c003a28d0d4afe1c550120ffe6f3ff701
    Signed-off-by: Taekgyun Ko <taeggyun.ko@samsung.com>

--- app/exynos_boot/exynos_boot.c
+++ app/exynos_boot/exynos_boot.c
@@ -1,7 +1,23 @@
+#include <debug.h>
+#include <reg.h>
 #include <app.h>
+#include <lib/console.h>
+#include <platform/sfr.h>
+#include <platform/fastboot.h>
+
+int cmd_boot(int argc, const cmd_args *argv);
 
 static void exynos_boot_task(const struct app_descriptor *app, void *args)
 {
+	unsigned int rst_stat = readl(EXYNOS9610_POWER_RST_STAT);
+
+	printf("RST_STAT: 0x%x\n", rst_stat);
+
+	if (rst_stat & (WARM_RESET | LITTLE_WDT_RESET | BIG_WDT_RESET))
+		do_fastboot(0, 0);
+	else
+		cmd_boot(0, 0);
+
 	return;
 }
 
