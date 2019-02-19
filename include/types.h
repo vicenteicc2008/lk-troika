0f64cfef7606d49511889f61934c9e13efd8100c
Kiwoong Kim <kwmad.kim@samsung.com>
2019-02-19 13:26:54 +0900

    include: add POSIX-like data type definitions
    To use __XX data type to share with external components
    
    Change-Id: I72371171f23c3cc657e010392bff65252197f63a
    Signed-off-by: Kiwoong Kim <kwmad.kim@samsung.com>

---
+++ include/types.h
@@ -0,0 +1,33 @@
+/*
+ * (C) Copyright 2019 SAMSUNG Electronics
+ * Kiwoong Kim <kwmad.kim@samsung.com>
+ *
+ * This software is proprietary of Samsung Electronics.
+ * No part of this software, either material or conceptual may be copied or distributed, transmitted,
+ * transcribed, stored in a retrieval system or translated into any human or computer language in any form by any means,
+ * electronic, mechanical, manual or otherwise, or disclosed
+ * to third parties without the express written permission of Samsung Electronics.
+ *
+ */
+
+#ifndef __PIT_TYPE_H__
+#define __PIT_TYPE_H__
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef unsigned char __u8;
+typedef unsigned short __u16;
+typedef unsigned int __u32;
+typedef unsigned long long __u64;
+
+typedef __u16  __le16;
+typedef __u16  __be16;
+typedef __u32  __le32;
+typedef __u32  __be32;
+typedef __u64  __le64;
+typedef __u64  __be64;
+
+#endif
