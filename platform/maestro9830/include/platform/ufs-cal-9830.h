935ad4ace01b82a03f6d7013c49c19d468c69993
dukhyun kwon <d_hyun.kwon@samsung.com>
2019-02-22 15:42:09 +0900

    [9830] ufs: add ufs CAL for exynos9830
    Change-Id: I3ad5530f7e7405dd5b6afbbe99fed9fb9faefc39
    Signed-off-by: dukhyun kwon <d_hyun.kwon@samsung.com>

--- platform/maestro9830/include/platform/ufs-cal-9830.h
+++ platform/maestro9830/include/platform/ufs-cal-9830.h
@@ -5,12 +5,30 @@ struct uic_pwr_mode {
 	u8 hs_series;
 };
 
+enum {
+	HOST_EMBD = 0,
+	HOST_CARD = 1,
+};
+
+enum {
+	GEAR_1 = 1,
+	GEAR_2,
+	GEAR_3,
+	GEAR_4,
+};
+
 struct ufs_cal_param {
 	void *host;		/* Host adaptor */
 	u8 available_lane;
-	u8 target_lane;
+	u8 connected_tx_lane;
+	u8 connected_rx_lane;
+	u8 active_tx_lane;
+	u8 active_rx_lane;
 	u32 mclk_rate;
+	u8 tbl;
 	u8 board;
+	u8 evt_ver;
+	u8 max_gear;
 	struct uic_pwr_mode *pmd;
 };
 
@@ -23,10 +41,19 @@ typedef enum {
 
 enum {
 	__BRD_SMDK,
+	__BRD_ASB,
+	__BRD_HSIE,
+	__BRD_ZEBU,
 	__BRD_UNIV,
+	__BRD_MAX,
 };
 #define BRD_SMDK	(1U << __BRD_SMDK)
+#define BRD_ASB		(1U << __BRD_ASB)
+#define BRD_HSIE	(1U << __BRD_HSIE)
+#define BRD_ZEBU	(1U << __BRD_ZEBU)
 #define BRD_UNIV	(1U << __BRD_UNIV)
+#define BRD_MAX		(1U << __BRD_MAX)
+#define BRD_ALL		((1U << __BRD_MAX) - 1)
 
 /* UFS CAL interface */
 ufs_cal_errno ufs_cal_post_h8_enter(struct ufs_cal_param *p);
