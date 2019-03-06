05bf50402cdbf69b7eaef16f2b573ef83542c437
Donghoon Yu <hoony.yu@samsung.com>
2019-03-06 19:41:09 +0900

    TARGET: SMDK9830: add board_info.h
    Change-Id: I155445d85b68b9348788be64bb5cae85ebd9b0e6
    Signed-off-by: Donghoon Yu <hoony.yu@samsung.com>

---
+++ target/smdk9830/include/target/board_info.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright@ Samsung Electronics Co. LTD
+ *
+ * This software is proprietary of Samsung Electronics.
+ * No part of this software, either material or conceptual may be copied or distributed, transmitted,
+ * transcribed, stored in a retrieval system or translated into any human or computer language in any form by any means,
+ * electronic, mechanical, manual or otherwise, or disclosed
+ * to third parties without the express written permission of Samsung Electronics.
+ */
+
+#ifndef __BOARD_INFO_H__
+#define __BOARD_INFO_H__
+
+#define CONFIG_BOARD_ID	0
+
+#endif /*__BOARD_INFO_H__ */
