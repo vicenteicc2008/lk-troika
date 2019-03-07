ed1af7e558db62d96f82fc45e0950aa571a7d466
Taekgyun Ko <taeggyun.ko@samsung.com>
2019-03-07 11:38:13 +0000

    TARGET: UNIVERSAL9830: add base code
    Change-Id: I82545fbe4d23ca61c07978fcc567b9bcebb6d2e9
    Signed-off-by: Taekgyun Ko <taeggyun.ko@samsung.com>

---
+++ target/universal9830/rules.mk
@@ -0,0 +1,19 @@
+LOCAL_DIR := $(GET_LOCAL_DIR)
+MODULE := $(LOCAL_DIR)
+
+PLATFORM := maestro9830
+
+MEMBASE := 0xFF000000
+MEMSIZE := 0x00F00000
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/target.c \
+	$(LOCAL_DIR)/dpu_io/dpu_gpio.S \
+	$(LOCAL_DIR)/dpu_io/dpu_io_ctrl.c \
+	$(LOCAL_DIR)/dpu_panels/s6e3fa0_lcd_ctrl.c \
+	$(LOCAL_DIR)/dpu_panels/s6e3fa0_mipi_lcd.c \
+	$(LOCAL_DIR)/dpu_panels/nt36672a_lcd_ctrl.c \
+	$(LOCAL_DIR)/dpu_panels/nt36672a_mipi_lcd.c \
+	$(LOCAL_DIR)/dpu_panels/lcd_module.c \
+
+include make/module.mk
