496613c91b64b0b3e46d9c08a35d1b83fb94761d
Taekgyun Ko <taeggyun.ko@samsung.com>
2019-03-07 11:43:11 +0000

    PROJECT: UNIVERSAL9830_BRINGUP: add base code
    Change-Id: Ib042d38c2cb0956676000eef1fdffc7d280194fb
    Signed-off-by: Taekgyun Ko <taeggyun.ko@samsung.com>

---
+++ project/universal9830_bringup.mk
@@ -0,0 +1,27 @@
+ARCH := arm64
+ARM_CPU := cortex-a53
+TARGET := universal9830
+
+WITH_KERNEL_VM := 0
+WITH_LINKER_GC := 1
+
+GLOBAL_DEFINES += \
+	SYSPARAM_ALLOW_WRITE=1 \
+	WITH_LIB_CONSOLE=1
+
+MODULES += \
+	app/exynos_boot
+
+MODULE_DEPS += \
+	lib/gpt \
+	lib/font \
+	lib/logo \
+	lib/libavb \
+	lib/cksum \
+	lib/fdt \
+	lib/ufdt \
+	dev/scsi \
+	dev/rpmb \
+	dev/boot \
+	dev/dpu \
+	dev/adc
