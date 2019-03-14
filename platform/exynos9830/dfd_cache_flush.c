d585c8131434a6805067d199a61c8b05dbcfe445
Changki Kim <changki.kim@samsung.com>
2019-03-14 10:22:34 +0900

    PLATFORM: EXYNOS9830: modified dfd to 1st draft
    Change-Id: If5fdf64fb34f2bb0eba1ed93c528e98cef667aa1
    Signed-off-by: Changki Kim <changki.kim@samsung.com>

--- platform/exynos9830/dfd_cache_flush.c
+++ platform/exynos9830/dfd_cache_flush.c
@@ -36,35 +36,42 @@
 #define L2TAG0_PA_39_17_MASK		(u64)0x01FFFFFC
 #define DL1TAG0_PA_39_12_MASK		(u64)0x0FFFFFFF
 
-#define L3_BANK_NUM			0x03
+#define L3_BANK_NUM			0x02
 #define L3_SUBBANK_NUM			0x04
-#define L3_TAG_WAYS_CONCATENATE		0x02
+#define L3_BANK_WIDTH			0x02
+#define L3_TAG_SECTORS_CONCATENATE	0x02
 #define L3_TAG_SIZE			12
 #define L3_DATA_SIZE			64
 #define L3_SET_INDEX_ADDR_LSB		8
 #define SET_END_L3			0x100
 #define WAY_END_L3			0x10
+#define SECTOR_END_L3			0x2
 #define OFFSET_END_L3			0x40
+#define OFFSET_WIDTH_L3			0x06
 #define OFFSET_STEP_NUM_L3		0x10
 #define OFFSET_STEP_WIDTH_L3		0x04
 
-#define L2_BANK_NUM			0x04
-#define SET_END_L2			0x800
+#define BANK_END_L2			0x4
+#define SET_END_L2			0x100
 #define WAY_END_L2			0x8
+#define SECTOR_END_L2			0x2
+#define L2_CHUNK			0x48
 #define OFFSET_END_L2			0x40
-#define OFFSET_WIDTH_L2			0x06
+#define OFFSET_WIDTH_L2			0x7
 #define OFFSET_STEP_NUM_L2		0x10
+#define OFFSET_STEP_WIDTH_L2		0x04
 
 #define SET_END_D			0x100
 #define WAY_END_D			0x4
 #define OFFSET_END_D			0x40
 #define OFFSET_WIDTH_D			0x06
 #define OFFSET_STEP_NUM_D		0x08
+#define OFFSET_STEP_WIDTH_D		0x03
 
-#define L3CACHE_DUMP_ADDR		(u32)debug_snapshot_get_item_paddr("log_cachedump")
+#define L3CACHE_DUMP_ADDR		(u32)debug_snapshot_get_item_paddr("log_arrdumpreset")
 #define L3CACHE_DUMP_SIZE		(u64)0x00500000
-#define L3CACHE_TAG_SIZE		(u64)(L3_BANK_NUM * L3_SUBBANK_NUM * SET_END_L3 *\
-					WAY_END_L3 / L3_TAG_WAYS_CONCATENATE * L3_TAG_SIZE)
+#define L3CACHE_TAG_SIZE		(u64)(L3_BANK_NUM * L3_SUBBANK_NUM * SET_END_L3 * \
+					WAY_END_L3 / L3_TAG_SECTORS_CONCATENATE * SECTOR_END_L3* L3_TAG_SIZE)
 #define L3CACHE_DATA_DUMP_ADDR		(u64)(L3CACHE_DUMP_ADDR + L3CACHE_TAG_SIZE)
 
 #define L2CACHE_DUMP_ADDR		(u64)(L3CACHE_DUMP_ADDR + L3CACHE_DUMP_SIZE)
@@ -94,9 +101,7 @@ extern u64 sec_area_end;
 
 struct DL1RAMINDEX_LO_BIT_INFO {
 	unsigned int INDEX:19;		//bit 18:0
-	unsigned int WAY:2;		//bit 20:19
-	unsigned int SET_MSB:1;		//bit 21
-	unsigned int DUMMY0:1;		//bit 22
+	unsigned int WAY:4;			//bit 22:19
 	unsigned int WORD_IDENTIFIER:2;	//bit 24:23
 	unsigned int DUMMY1:2;		//bit 25:26
 	unsigned int RAMID:5;		//bit 31:27
@@ -109,7 +114,7 @@ struct DL1RAMINDEX_HI_BIT_INFO {
 
 struct DL1TAG0_BIT_INFO {
 	unsigned int PHY_ADD_39_12:28;	//bit 27:0
-	unsigned int NS:1;		//bit 28
+	unsigned int NS:1;			//bit 28
 	unsigned int DUMMY0:3;		//bit 31:29
 };
 
@@ -119,16 +124,17 @@ struct DL1TAG1_BIT_INFO {
 };
 
 struct L3RAMINDEX_LO_BIT_INFO {
-	unsigned int DUMMY0:2;		//bit 1:0
+	unsigned int DUMMY0:1;		//bit 0
+	unsigned int SECTOR:1;		//bit 1
 	unsigned int SUBBANK:2;		//bit 3:2
 	unsigned int QWORD:2;		//bit 5:4
 	unsigned int BANK:2;		//bit 7:6
 	unsigned int L3INDEX:8;		//bit 15:8
 	unsigned int DUMMY1:3;		//bit 18:16
-	unsigned int WAY:4;		//bit 22:19
+	unsigned int WAY:4;			//bit 22:19
 	unsigned int DUMMY2:2;		//bit 24:23
 	unsigned int WRITE:1;		//bit 25
-	unsigned int DUMMY3:1;		//bit 26
+	unsigned int CONTROL:1;		//bit 26
 	unsigned int RAMID:5;		//bit 31:27
 };
 
@@ -138,13 +144,16 @@ struct L3RAMINDEX_HI_BIT_INFO {
 };
 
 struct L3TAG_BIT_INFO {
-	u64 MOESI:3;			//bit 2:0
-	u64 CPUID:4;			//bit 6:3
-	u64 Inclusive:1;		//bit 7
-	u64 NS:1;			//bit 8
-	u64 PA_7_6:2;			//bit 10:9
-	u64 PA_39_16:24;		//bit 34:11
-	u64 ECC:7;			//bit 41:35
+	u64 MOESI0:3;			//bit 2:0
+	u64 MOESI1:3;			//bit 5:3
+	u64 SL2ID0:2;			//bit 7:6
+	u64 SL2ID1:2;			//bit 9:8
+	u64 NS:1;				//bit 10
+	u64 PA_7:1;				//bit 11
+	u64 PA_39_16:24;		//bit 35:12
+	u64 REREF:1;			//bit 36
+	u64 PREFETCH:1;			//bit 37
+	u64 ECC:7;				//bit 44:38
 };
 
 struct L2RAMINDEX_LO_BIT_INFO {
@@ -152,9 +161,10 @@ struct L2RAMINDEX_LO_BIT_INFO {
 	unsigned int QWORD:2;		//bit 5:4
 	unsigned int BANK:2;		//bit 7:6
 	unsigned int L2INDEX:9;		//bit 16:8
-	unsigned int L2PARTITION:2;	//bit 18:17
-	unsigned int WAY:4;		//bit 22:19
-	unsigned int DUMMY1:4;		//bit 26:23
+	unsigned int DUMMY1:2;		//bit 18:17
+	unsigned int WAY:4;			//bit 22:19
+	unsigned int SECTOR:1;		//bit 23
+	unsigned int DUMMY2:3;		//bit 26:24
 	unsigned int RAMID:5;		//bit 31:27
 };
 
@@ -164,18 +174,22 @@ struct L2RAMINDEX_HI_BIT_INFO {
 };
 
 struct L2TAG0_BIT_INFO {
-	unsigned int MESI:3;		//bit 2:0
-	unsigned int CP:1;		//bit 3
-	unsigned int LOCK:1;		//bit 4
-	unsigned int PA_8_6:3;		//bit 7:5
-	unsigned int DUMMY:1;		//bit 8
-	unsigned int PA_39_18:22;	//bit 30:9
-	unsigned int NS:1;		//bit 31
+	unsigned int MESI0:3;		//bit 2:0
+	unsigned int CPUID0:2;		//bit 4:3
+	unsigned int LOCK0:1;		//bit 5
+	unsigned int DUMMY0:4;		//bit 9:6
+	unsigned int MESI1:3;		//bit 12:10
+	unsigned int CPUID1:2;		//bit 14:13
+	unsigned int LOCK1:1;		//bit 15
+	unsigned int DUMMY1:4;		//bit 19:16
+	unsigned int NS:1;			//bit 20
+	unsigned int PA_9_7:3;		//bit 23:21
+	unsigned int PA_23_16:8;	//bit 31:24
 };
 
 struct L2TAG1_BIT_INFO {
-	unsigned int LS_VA13_12:2;	//bit 33:32
-	unsigned int res:31;
+	unsigned int PA_39_24:16;	//bit 15:0
+	unsigned int res:16;
 };
 
 union DL1RAMINDEX_LO_REG {
@@ -262,43 +276,49 @@ static int is_dram_bound(u64 addr)
 	return 0;
 }
 
-static void dump_l3_operation(u64 bank, u64 subbank, u64 set, u64 way)
+static void dump_l3_operation(u64 bank, u64 subbank, u64 set, u64 way, u64 sector)
 {
 	union L3TAG l3tag_reg;
 	u64 l3addr_full;
 	u64 l3_tag_raw_data0, l3_tag_raw_data1, l3_tag_raw_data2;
 	u64 dump_base;
+	u64 MOESI;
 
-	dump_base = L3CACHE_DUMP_ADDR + bank * L3_SUBBANK_NUM * SET_END_L3 *
-		(WAY_END_L3 / L3_TAG_WAYS_CONCATENATE) * L3_TAG_SIZE;
-	dump_base += subbank * SET_END_L3 * (WAY_END_L3 / L3_TAG_WAYS_CONCATENATE) * L3_TAG_SIZE;
-	dump_base += set * (WAY_END_L3 / L3_TAG_WAYS_CONCATENATE) * L3_TAG_SIZE;
-	dump_base += (way / L3_TAG_WAYS_CONCATENATE) * L3_TAG_SIZE;
+	dump_base = L3CACHE_DUMP_ADDR + bank * L3_SUBBANK_NUM *
+			SET_END_L3 * WAY_END_L3 * L3_TAG_SIZE;
+	dump_base += subbank * SET_END_L3 * WAY_END_L3 * L3_TAG_SIZE;
+	dump_base += set * WAY_END_L3 * L3_TAG_SIZE;
+	dump_base += way * L3_TAG_SIZE;
 
 	l3_tag_raw_data0 = readl(dump_base + 0x0);
 	l3_tag_raw_data1 = readl(dump_base + 0x4);
 	l3_tag_raw_data2 = readl(dump_base + 0x8);
 
 	l3tag_reg.ll_word = 0;
-	if((way % L3_TAG_WAYS_CONCATENATE) == 0)
-	    l3tag_reg.ll_word = ((l3_tag_raw_data1 & 0x3ff) << 32) | (l3_tag_raw_data0);
-	else
-	    l3tag_reg.ll_word = ((l3_tag_raw_data2 & 0xfffff) << 22) | (l3_tag_raw_data1 >> 10);
+	if(sector == 0) {
+	    l3tag_reg.ll_word = ((l3_tag_raw_data1 & 0x1fff) << 32) | l3_tag_raw_data0;
+	    MOESI=l3tag_reg.bits.MOESI0;
+	} else {
+	    l3tag_reg.ll_word = ((l3_tag_raw_data2 & 0x3ffffff) << 19)| (l3_tag_raw_data1 >> 13);
+	    MOESI=l3tag_reg.bits.MOESI1;
+	}
 
-	/* determine writeback. modified or owned */
-	if (!(((l3tag_reg.bits.MOESI & (MOESI_VALID | MOESI_MODIFIED)) ==
-	     (MOESI_VALID | MOESI_MODIFIED)) && ((l3tag_reg.bits.NS) == 0x1)))
+	/* determine writeback */
+	if (!(((MOESI & (MOESI_VALID | MOESI_MODIFIED)) == (MOESI_VALID | MOESI_MODIFIED))
+			&& ((l3tag_reg.bits.NS) == 0x1)))
 		return;
 
 	l3addr_full = (((u64) l3tag_reg.bits.PA_39_16) << 16) |
 		((u64) set << L3_SET_INDEX_ADDR_LSB) |
-		((u64) l3tag_reg.bits.PA_7_6 << 6);
+		((u64) l3tag_reg.bits.PA_7 << 7) |
+		((u64) sector << 6);
 
 	dump_base = L3CACHE_DATA_DUMP_ADDR + bank * L3_SUBBANK_NUM * SET_END_L3 *
-		WAY_END_L3*L3_DATA_SIZE;
-	dump_base += subbank * SET_END_L3 * WAY_END_L3 * L3_DATA_SIZE;
-	dump_base += set * WAY_END_L3 * L3_DATA_SIZE;
-	dump_base += way * L3_DATA_SIZE;
+				WAY_END_L3 * SECTOR_END_L3 * L3_DATA_SIZE;
+	dump_base += subbank * SET_END_L3 * WAY_END_L3 * SECTOR_END_L3 * L3_DATA_SIZE;
+	dump_base += set * WAY_END_L3 * SECTOR_END_L3 * L3_DATA_SIZE;
+	dump_base += way * SECTOR_END_L3 * L3_DATA_SIZE;
+	dump_base += sector * L3_DATA_SIZE;
 
 	for (u32 offset = 0; offset < OFFSET_END_L3; offset += OFFSET_STEP_NUM_L3) {
 		u32 l3data0 = readl(dump_base + offset + 0x0);
@@ -326,7 +346,7 @@ static void dump_l3_operation(u64 bank, u64 subbank, u64 set, u64 way)
 
 static void write_back_l3_cache(int core)
 {
-	u64 bank, subbank, set, way;
+	u64 bank, subbank, set, way, sector;
 
 	if (core < 0 || core > BIG_NR_CPUS - 1)
 		return;
@@ -335,26 +355,40 @@ static void write_back_l3_cache(int core)
 		for (subbank = 0; subbank < L3_SUBBANK_NUM; subbank++)
 			for (set = 0; set < SET_END_L3; set++)
 				for (way = 0; way < WAY_END_L3; way++)
-					dump_l3_operation(bank, subbank, set, way);
+					for (sector = 0; sector < SECTOR_END_L3; sector++)
+						dump_l3_operation(bank, subbank, set, way ,sector);
+
 }
 
-static void dump_l2_operation(u64 core, u64 set_index, u64 way_index)
+static void dump_l2_operation(u64 core, u64 bank, u64 set, u64 way, u64 sector)
 {
 	union L2TAG0 l2tag0_reg;
+	union L2TAG1 l2tag1_reg;
 	u64 l2addr_full;
-	u64 dump_base = CORE_DUMP_ADDR + (CORE_DUMP_SIZE*core) +
-			(set_index*WAY_END_L2 + way_index)*L2_LINE_RAW_DATA_SIZE;
+	u64 dump_base;
+	u64 MESI;
+
+	dump_base = CORE_DUMP_ADDR + (set * WAY_END_L2*SECTOR_END_L2 +
+			way * SECTOR_END_L2 + sector) * L2_LINE_RAW_DATA_SIZE;
 
 	l2tag0_reg.word = readl(dump_base + 0x0);
+	l2tag1_reg.word = readl(dump_base + 0x4);
+
+	if (sector == 0)
+		MESI=l2tag0_reg.bits.MESI0;
+	else
+		MESI=l2tag0_reg.bits.MESI1;
 
 	/* determine writeback. modified or owned */
-	if (!(((l2tag0_reg.bits.MESI & (L2_MESI_VALID | L2_MESI_MODIFIED)) ==
-	     (L2_MESI_VALID | L2_MESI_MODIFIED)) && ((l2tag0_reg.bits.NS) == 0x1)))
+	if (!(((MESI & (L2_MESI_VALID | L2_MESI_MODIFIED)) == (L2_MESI_VALID | L2_MESI_MODIFIED))
+			&& ((l2tag0_reg.bits.NS) == 0x1)))
 		return;
 
-	l2addr_full = (((u64) l2tag0_reg.bits.PA_39_18) << 18) |
-		((set_index/L2_BANK_NUM) << 9) |
-		((u64) l2tag0_reg.bits.PA_8_6 << OFFSET_WIDTH_L2);
+	l2addr_full = (((u64) l2tag1_reg.bits.PA_39_24) << 24) |
+		(((u64) l2tag0_reg.bits.PA_23_16) << 16) |
+		(((u64) (set & 0x3f) << 10)) |
+		((u64) l2tag0_reg.bits.PA_9_7 << OFFSET_WIDTH_L2) |
+		((u64) sector << 6);
 
 	for (u32 offset = 0; offset < OFFSET_END_L2; offset += OFFSET_STEP_NUM_L2) {
 		u32 l2data0 = readl(dump_base + L2_TAG_DATA_SIZE + offset + 0x0);
@@ -364,7 +398,7 @@ static void dump_l2_operation(u64 core, u64 set_index, u64 way_index)
 		u64 l2addr = l2addr_full + offset;
 #ifdef DEBUG_PRINT
 		printf("[L2] PA:0x%016llx: 0x%08x 0x%08x 0x%08x 0x%08x (set:0x%x, way:0x%x)\n",
-				l2addr, l2data0, l2data1, l2data2, l2data3, set_index, way_index);
+				l2addr, l2data0, l2data1, l2data2, l2data3, set, way);
 #endif
 		if (is_sec_area_bound(l2addr))
 			continue;
@@ -381,17 +415,19 @@ static void dump_l2_operation(u64 core, u64 set_index, u64 way_index)
 
 static void write_back_l2_cache(int core)
 {
-	u64 set, way;
+	u64 bank, set, way, sector;
 
 	if (core < 0 || core > BIG_NR_CPUS - 1)
 		return;
 
-	for (set = 0; set < SET_END_L2; set++)
-		for (way = 0; way < WAY_END_L2; way++)
-			dump_l2_operation(core, set, way);
+	for (bank = 0; bank < BANK_END_L2; bank++)
+		for (set = 0; set < SET_END_L2; set++)
+			for (way = 0; way < WAY_END_L2; way++)
+				for (sector = 0; sector < SECTOR_END_L2; sector++)
+					dump_l2_operation(core, bank, set, way, sector);
 }
 
-static void write_back_d_cache_operation(u32 core, u64 set_index, u64 way_index)
+static void write_back_d_cache_operation(u32 core, u64 set, u64 way)
 {
 	union DL1TAG0 dl1tag0_reg;
 	union DL1TAG1 dl1tag1_reg;
@@ -399,17 +435,18 @@ static void write_back_d_cache_operation(u32 core, u64 set_index, u64 way_index)
 	u64 dump_base;
 
 	dump_base = CORE_DUMP_ADDR + CORE_DUMP_SIZE * core + L2DATA_RAW_SIZE +
-			(set_index * WAY_END_D + way_index) * DL1_LINE_RAW_DATA_SIZE;
+			(set * WAY_END_D + way) * DL1_LINE_RAW_DATA_SIZE;
 
 	dl1tag0_reg.word = readl(dump_base + 0x0);
 	dl1tag1_reg.word = readl(dump_base + 0x4);
+
 	/* determine writeback. modified or owned */
 	if (!(((dl1tag1_reg.bits.MESI & (MESI_VALID | MESI_MODIFIED)) ==
 		(MESI_VALID | MESI_MODIFIED)) && (dl1tag0_reg.bits.NS != 0)))
 		return;
 
 	daddr_full = (((u64)dl1tag0_reg.word & DL1TAG0_PA_39_12_MASK) << 12) |
-		(((set_index & 0x3f) << OFFSET_WIDTH_D) & 0x0fff);
+		(((set & 0x3f) << OFFSET_WIDTH_D) & 0x0fff);
 
 	for (u32 offset = 0; offset < OFFSET_END_D; offset += OFFSET_STEP_NUM_D) {
 		u32 data0 = readl(dump_base + DL1_TAG_DATA_SIZE + offset + 0x0);
@@ -417,7 +454,7 @@ static void write_back_d_cache_operation(u32 core, u64 set_index, u64 way_index)
 		u64 daddr = daddr_full + offset;
 #ifdef DEBUG_PRINT
 		printf("[DL1-%d] PA:0x%016llx: 0x%08x 0x%08x (set:0x%x, way:0x%x)\n",
-				core, daddr, data0, data1, set_index, way_index);
+				core, daddr, data0, data1, set, way);
 #endif
 		if (is_sec_area_bound(daddr))
 			continue;
