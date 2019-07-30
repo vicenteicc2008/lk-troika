8d82c7e7eb466147e6b4aa340dfed6439e3aead9
Taekgyun Ko <taeggyun.ko@samsung.com>
2019-07-30 04:53:22 +0000

    LIB: LOCK: add common header file
    Change-Id: I3bd08613a3d6b351d94708d328232b8ab79c671f
    Signed-off-by: Taekgyun Ko <taeggyun.ko@samsung.com>

---
+++ include/lib/lock.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright@ Samsung Electronics Co. LTD
+ *
+ * This software is proprietary of Samsung Electronics.
+ * No part of this software, either material or conceptual may be copied or distributed, transmitted,
+ * transcribed, stored in a retrieval system or translated into any human or computer language in any form by any means,
+ * electronic, mechanical, manual or otherwise, or disclosed
+ * to third parties without the express written permission of Samsung Electronics.
+ */
+
+#ifndef __LOCK_H__
+#define __LOCK_H__
+
+#define LOCK_SYSPARAM_NAME		"locked"
+#define LOCK_CRITICAL_SYSPARAM_NAME	"lock_critical"
+
+void lock(int state);
+int get_lock_state(void);
+void lock_critical(int state);
+int get_unlock_ability(void);
+
+#endif	/* __LOCK_H__ */
