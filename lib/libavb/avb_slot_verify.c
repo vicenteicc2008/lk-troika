9a74773c741a41d7217cbbe6acad9d273ec8cab7
Jungi Lee <jungilsi.lee@samsung.com>
2019-10-29 20:08:16 +0900

    lib: libavb: Set security patch level from vbmeta
    Change-Id: I4a7b8524b43fd70a22101088b96304dac8197506
    Signed-off-by: Jungi Lee <jungilsi.lee@samsung.com>

--- lib/libavb/avb_slot_verify.c
+++ lib/libavb/avb_slot_verify.c
@@ -1182,8 +1182,16 @@ static AvbSlotVerifyResult load_and_verify_vbmeta(
       } break;
 
       case AVB_DESCRIPTOR_TAG_PROPERTY:
-        /* Do nothing. */
+	{
+	AvbPropertyDescriptor *prop_desc = (AvbPropertyDescriptor *)descriptors[n];
+	uint64_t key_num_bytes = avb_be64toh(prop_desc->key_num_bytes);
+	uint64_t value_num_bytes = avb_be64toh(prop_desc->value_num_bytes);
+	char *key = (char *)((uint64_t)prop_desc + sizeof(AvbPropertyDescriptor));
+	char *value = (char *)((uint64_t)prop_desc + sizeof(AvbPropertyDescriptor)
+			+ key_num_bytes + 1);
+	avb_set_patch_level(key, value, value_num_bytes);
         break;
+	}
     }
   }
 
