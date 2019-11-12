4bda79cbb50a56807fcdc2a73e350c1514d202eb
Kijun Kim <kijun.kim@samsung.com>
2019-11-12 16:50:50 +0900

    Revert "[HACK][850] disable LK display feature".
    This reverts commit 4ee7489c34ff048d31630d83af88e181b841fddd.
    
    Change-Id: I590d95a6b8100d2ff0a353a6f59350d672161e27

--- target/universal3830/dpu_panels/lcd_module.c
+++ target/universal3830/dpu_panels/lcd_module.c
@@ -100,7 +100,7 @@ int cm_fill_id(struct dsim_device *dsim)
 	return 0;
 }
 
-u32 ddi_id = 0x34090;
+u32 ddi_id;
 /* read ddi's id(panel_ids[]) for matching expected id */
 int cm_read_id(struct dsim_device *dsim)
 {
