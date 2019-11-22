bb286bc12fc8ba662cf1ac947c8727d45988fce6
Taekgyun Ko <taeggyun.ko@samsung.com>
2019-11-22 10:21:41 +0000

    LIB: LOCK: define common APIs that use RPMB instead of ENV to store lock state
    Change-Id: Ia56184d6e564b821827ae01e23580d8bad030028
    Signed-off-by: Taekgyun Ko <taeggyun.ko@samsung.com>

--- lib/lock/lock.c
+++ lib/lock/lock.c
@@ -10,32 +10,8 @@
 
 #include <debug.h>
 #include <stdlib.h>
-#include <part_gpt.h>
-#include <pit.h>
-#include <part.h>
-#include <platform/environment.h>
-
-void lock(int state)
-{
-	unsigned int *env_val;
-	struct pit_entry *ptn;
-
-	ptn = pit_get_part_info("env");
-	env_val = memalign(0x1000, part_get_size_in_bytes(ptn));
-	pit_access(ptn, PIT_OP_LOAD, (u64)env_val, 0);
-
-	/* The device should prompt users to warn them that
-	 * they may encounter problems with unofficial images.
-	 * After acknowledging, a factory data reset should be
-	 * done to prevent unauthorized data access. */
-	if ((env_val[ENV_ID_LOCKED] == 1) && (state == 0)) {
-	}
-
-	env_val[ENV_ID_LOCKED] = state;
-	pit_access(ptn, PIT_OP_FLASH, (u64)env_val, 0);
-
-	free(env_val);
-}
+#include <dev/rpmb.h>
+#include <platform/sfr.h>
 
 int set_lock_state(int state)
 {
@@ -58,47 +34,34 @@ int set_lock_state(int state)
 
 int get_lock_state(void)
 {
-	unsigned int *env_val;
-	struct pit_entry *ptn;
-	int lock_state;
+	int ret;
 
-	ptn = part_get("env");
-	env_val = memalign(0x1000, part_get_size_in_bytes(ptn));
-	part_read(ptn, env_val);
+#if defined(CONFIG_USE_RPMB)
+	int rpmb_ret;
+	uint32_t lock_state = 0;
+	rpmb_ret = rpmb_get_lock_state(&lock_state);
+	if (rpmb_ret == RV_SUCCESS) {
+		ret = lock_state;
+	} else {
+		ret = -1;
+	}
+#else
+	ret = 0;
+#endif
 
-	lock_state = env_val[ENV_ID_LOCKED];
-
-	free(env_val);
-
-	return lock_state;
+	return ret;
 }
 
 void lock_critical(int state)
 {
-	unsigned int *env_val;
-	struct pit_entry *ptn;
-
-	ptn = part_get("env");
-	env_val = memalign(0x1000, part_get_size_in_bytes(ptn));
-	part_read(ptn, env_val);
-
-	/* Transitioning from locked to unlocked state should
-	 * require a physical interaction with the device. */
-	if ((env_val[ENV_ID_LOCKED_CRITICAL] == 1) && (state == 0)) {
-	}
-
-	env_val[ENV_ID_LOCKED_CRITICAL] = state;
-	part_write(ptn, env_val);
-
-	free(env_val);
 }
 
 int get_unlock_ability(void)
 {
 	/* If get_unlock_ability is "0" the user needs to boot
-	 * to the home screen, go into the Settings > System > Developer
-	 * options menu and enable the OEM unlocking option to set
-	 * unlock_ability to: "1" */
+	  to the home screen, go into the Settings > System > Developer
+	  options menu and enable the OEM unlocking option to set
+	  unlock_ability to: "1" */
 	int unlock_ability = 1;
 
 	return unlock_ability;
