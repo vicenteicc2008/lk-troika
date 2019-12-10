c360f4c26d6f8276cc5c504156aab1030f69c871
junbo.sim <junbo.sim@samsung.com>
2019-12-10 19:42:11 +0900

    [980] Add Board revision boot arguments
    Change-Id: I01a50717ed67e815330576f4a5902dc61f07dee3
    Signed-off-by: junbo.sim <junbo.sim@samsung.com>

--- platform/exynos9630/boot/cmd_boot.c
+++ platform/exynos9630/boot/cmd_boot.c
@@ -341,6 +341,14 @@ static int bootargs_process(void)
 		print_lcd_update(FONT_GREEN, FONT_BLACK, "AB Slot suffix set %s", buf);
 	}
 
+	/* Add Board_rev to bootargs */
+	memset(buf, 0, sizeof(buf));
+	snprintf(buf, 5, "0x%x", board_rev);
+	if (add_val("revision", buf)) {
+		printf("Board Revision set failed\n");
+		return -1;
+	}
+
 	/* Recovery */
 	if ((readl(EXYNOS_POWER_SYSIP_DAT0) == REBOOT_MODE_RECOVERY) ||
 		   (readl(EXYNOS_POWER_SYSIP_DAT0) == REBOOT_MODE_FASTBOOT_USER)) {
