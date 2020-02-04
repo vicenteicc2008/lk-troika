92e6bfd30014f85f5dbc43c9066dbd02b1a1048e
Sehwan Jeon <sehwan.jeon@samsung.com>
2020-02-04 20:10:15 +0900

    [3830] platform: exynos3830: display acpm_version info
    Change-Id: Ib99644b3f484b4917a7131878c86ed77bde6af42
    Signed-off-by: Sehwan Jeon <sehwan.jeon@samsung.com>

--- platform/exynos3830/platform.c
+++ platform/exynos3830/platform.c
@@ -30,6 +30,7 @@
 #include <platform/mmu/mmu_func.h>
 #include <lib/fastboot.h>
 #include <platform/sfr.h>
+#include <platform/acpm.h>
 #include <platform/uart.h>
 #include <platform/board_rev.h>
 #include <dev/mmc.h>
@@ -277,12 +278,42 @@ void platform_early_init(void)
 extern bool is_xct_boot(void);
 extern int init_fastboot_variables(void);
 
+void display_acpm_version(void)
+{
+	unsigned int i;
+	unsigned int plugins, num_plugins, plugin_address, plugin_ops_address;
+	struct build_info *info;
+
+	/* Check ACPM STACK Magic */
+	if (readl(EXYNOS3830_ACPM_MAGIC) != 0xC000)
+		return ;
+
+	info = (struct build_info *)(EXYNOS3830_ACPM_APSHARE + APSHARE_BUILDINFO_OFFSET);
+	printf("ACPM: Framework's  version is %s %s\n", info->build_version,
+							info->build_time);
+
+	plugins = readl(EXYNOS3830_ACPM_APSHARE);
+	num_plugins = readl(EXYNOS3830_ACPM_APSHARE + 4);
+
+	for (i = 1; i < num_plugins; i++) {
+		plugin_address = plugins + sizeof(struct plugin) * i;
+		if (readl(get_acpm_plugin_element(plugin, is_attached)) == 1) {
+			plugin_ops_address = readl(get_acpm_plugin_element(plugin, plugin_ops));
+			info = (struct build_info *)((char *)get_acpm_plugin_element(plugin_ops, info));
+			printf("ACPM: Plugin(id:%d) version is %s %s\n",
+					(int)readl(get_acpm_plugin_element(plugin, id)),
+					info->build_version, info->build_time);
+		}
+	}
+}
+
 void platform_init(void)
 {
 	unsigned int rst_stat = readl(EXYNOS3830_POWER_RST_STAT);
 	int ret = 0;
 
 	display_flexpmu_dbg();
+	display_acpm_version();
 
 	display_rst_stat(rst_stat);
 #if defined(CONFIG_AB_UPDATE)
